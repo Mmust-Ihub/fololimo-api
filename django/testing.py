d1c769dc7aac9af152c2c712d11fdcf81c604a12
Mikiejoe <sitb01-02668201@student.mmust.ac.ke>
2024-10-11 16:27:24 +0300

    update ussd menu and populate agrovets

---
+++ django/testing.py
@@ -0,0 +1,10 @@
+import random
+
+# Define the range
+lower_bound = 0  # inclusive
+upper_bound = 1  # inclusive
+
+# Generate a random integer within the specified range
+random_number = random.randint(lower_bound, upper_bound)
+
+print(random_number)
