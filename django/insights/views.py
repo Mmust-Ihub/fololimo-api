a40e1a5fa940b7a40611da242464ee39d0d17078
Mikiejoe <sitb01-02668201@student.mmust.ac.ke>
2024-10-12 20:09:53 +0300

    resolving bugs

--- django/insights/views.py
+++ django/insights/views.py
@@ -1,6 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
+from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from .models import Farm
 from .serializers import FarmSerializer
@@ -25,14 +26,19 @@ class FarmViewSet(ModelViewSet):
         return Response(serializer.data)
     
     def create(self, request):
-        user = request.user
-        data = request.data
-        data["user"] = user.id
-        print("Data: ", data)
-        serializer = FarmSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=201)
-        return Response(serializer.errors, status=400)
+        try:
+            user = request.user
+            data = request.data
+            data["user"] = user
+            print("Data: ", data)
+            serializer = FarmSerializer(data=data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=201)
+            return Response(serializer.errors, status=400)
+        except Exception as e:
+            return Response({
+                "message": "something went wrong"
+            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
