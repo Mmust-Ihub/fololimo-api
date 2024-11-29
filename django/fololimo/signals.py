4f4b5a72f713d5b17bce41a71736b4957f5eca0c
Mikiejoe <sitb01-02668201@student.mmust.ac.ke>
2024-11-29 19:43:41 +0300

    remove sms function

--- django/fololimo/signals.py
+++ django/fololimo/signals.py
@@ -1,11 +1,11 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import Message
-from .send_sms import send_sms
+# from .send_sms import send_sms
 
 
 @receiver(post_save, sender=Message)
 def send_sms_siganal(sender, instance, **kwargs):
     if kwargs['created']:
-        print()
-        send_sms(instance.location.sub_county, instance.message)
+        print("sms sent")
+        # send_sms(instance.location.sub_county, instance.message)
