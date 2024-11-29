81c5e73c3a4c4885b6bd3a07d8047f6544ec831a
Mikiejoe <sitb01-02668201@student.mmust.ac.ke>
2024-11-29 20:05:19 +0300

    remove signals

--- django/fololimo/apps.py
+++ django/fololimo/apps.py
@@ -5,5 +5,5 @@ class FololimoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'fololimo'
 
-    def ready(self):
-        import fololimo.signals
+    # def ready(self):
+    #     import fololimo.signals
